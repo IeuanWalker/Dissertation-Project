6c8e622ba7da2274d9109c7a37001853d7576bdc
Ieuan Walker <ieuan.walker007@gmail.com>
2017-04-22 16:26:33 +0100

    Code optimised and cleaned

--- The application/SemanticWebNPLSearchEngine/Scripts/connector.js
+++ The application/SemanticWebNPLSearchEngine/Scripts/connector.js
@@ -1,6 +1,6 @@
 //https://codepen.io/florian/details/gAxtK
 
-$(document).ready(function(){
+$(document).ready(function () {
     dotGenerator(0, true);
     dotGenerator(1, false);
 });
@@ -23,23 +23,23 @@ function dotGenerator(i, linesOrNot) {
         this.r = this.value(min);
         this.g = this.value(min);
         this.b = this.value(min);
-        this.style = "rgba( 0, "+ this.g +","+ this.b +",0.3)";
+        this.style = "rgba( 0, " + this.g + "," + this.b + ",0.3)";
     }
 
     Color.prototype = {
-        value: function(min) {
-            return Math.floor(Math.random()*255 + min);
+        value: function (min) {
+            return Math.floor(Math.random() * 255 + min);
         }
     }
 
     function mixComponents(comp1, comp2, weight1, weight2) {
-        return (comp1*weight1 + comp2*weight2) / (weight1 + weight2);
+        return (comp1 * weight1 + comp2 * weight2) / (weight1 + weight2);
     }
 
     function gradient(dot1, dot2, midColor) {
         var grad = ctx.createLinearGradient(Math.floor(dot1.x), Math.floor(dot1.y), Math.floor(dot2.x), Math.floor(dot2.y));
         grad.addColorStop(0, dot1.color.style);
-        grad.addColorStop(Math.floor(dot1.radius / (dot1.radius/dot2.radius)), midColor);
+        grad.addColorStop(Math.floor(dot1.radius / (dot1.radius / dot2.radius)), midColor);
         grad.addColorStop(1, dot2.color.style);
         return grad;
     }
@@ -54,8 +54,8 @@ function dotGenerator(i, linesOrNot) {
     }
 
     var connectArea = {
-        x: 50*canvas.width / 100,
-        y: 50*canvas.height / 100
+        x: 50 * canvas.width / 100,
+        y: 50 * canvas.height / 100
     };
 
     var numberOfDots = 0;
@@ -78,10 +78,10 @@ function dotGenerator(i, linesOrNot) {
     }
 
     Dot.prototype = {
-        draw: function() {
+        draw: function () {
             ctx.beginPath();
             ctx.fillStyle = this.color.style;
-            ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false);
+            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
             ctx.fill();
         }
     };
@@ -92,7 +92,7 @@ function dotGenerator(i, linesOrNot) {
 
             if (dot.y < 0 || dot.y > canvas.height) {
                 dot.vy = - dot.vy;
-            } else if(dot.x < 0 || dot.x > canvas.width) {
+            } else if (dot.x < 0 || dot.x > canvas.width) {
                 dot.vx = - dot.vx;
             }
             dot.x += dot.vx;
@@ -101,7 +101,7 @@ function dotGenerator(i, linesOrNot) {
     }
 
     //If u dont want to connect the lines
-    if(linesOrNot){
+    if (linesOrNot) {
         function connectDots() {
             for (var i = 0; i < dots.nb; i++) {
                 for (var j = 0; j < dots.nb; j++) {
@@ -115,10 +115,10 @@ function dotGenerator(i, linesOrNot) {
                     var xCoreDiff = dot1.x - connectArea.x;
                     var yCoreDiff = dot1.y - connectArea.y;
 
-                    if ( (xDiff < dots.distMax && xDiff > -dots.distMax)
+                    if ((xDiff < dots.distMax && xDiff > -dots.distMax)
                         && (yDiff < dots.distMax && yDiff > -dots.distMax)
                         && (xCoreDiff < dots.connectAreaRadius && xCoreDiff > -dots.connectAreaRadius)
-                        && (yCoreDiff < dots.connectAreaRadius && yCoreDiff > -dots.connectAreaRadius) ) {
+                        && (yCoreDiff < dots.connectAreaRadius && yCoreDiff > -dots.connectAreaRadius)) {
                         ctx.beginPath();
                         ctx.strokeStyle = lineStyle(dot1, dot2);
                         ctx.moveTo(dot1.x, dot1.y);
@@ -129,20 +129,20 @@ function dotGenerator(i, linesOrNot) {
                 }
             }
         }
-        $("canvas").on("mousemove", function(e) {
+        $("canvas").on("mousemove", function (e) {
             connectArea.x = e.pageX;
             connectArea.y = e.pageY;
         });
     }
 
     function createDots() {
-        for(i = 0; i < dots.nb; i++) {
+        for (i = 0; i < dots.nb; i++) {
             dots.array.push(new Dot());
         }
     }
 
     function drawDots() {
-        for(i = 0; i < dots.nb; i++) {
+        for (i = 0; i < dots.nb; i++) {
             dot = dots.array[i];
             dot.draw();
         }
@@ -151,7 +151,7 @@ function dotGenerator(i, linesOrNot) {
     function animateDots() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         moveDots();
-        if(linesOrNot){
+        if (linesOrNot) {
             connectDots();
         }
         drawDots();
