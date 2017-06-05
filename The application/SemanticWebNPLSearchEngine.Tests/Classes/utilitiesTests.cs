8fdb16bd75fe291ae64c3a2be1597cf64c5792d6
Ieuan Walker <ieuan.walker007@gmail.com>
2017-06-05 14:59:17 +0100

    Experimenting with unit testing

---
+++ The application/SemanticWebNPLSearchEngine.Tests/Classes/utilitiesTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using SemanticWebNPLSearchEngine.Models;
+
+namespace SemanticWebNPLSearchEngine.Classes.Tests
+{
+    [TestClass()]
+    public class utilitiesTests
+    {
+        [TestMethod()]
+        public void RemoveLast3Cahracters()
+        {
+            string test = "Hello World@en";
+            string expected = "Hello World";
+
+            string output = utilities.RemoveLast3Cahracters(test);
+
+            Assert.AreEqual(expected, output);
+        }
+        [TestMethod()]
+        public void DateCreatorTest()
+        {
+            string test = "08/06/1996^^Date";
+            string expected = "08/06/1996";
+
+            string output = utilities.DateCreator(test);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod()]
+        public void ExtractLuisDataTest()
+        {
+            string limit = String.Format("LIMIT({0})", 10);
+            string genreMatch = String.Format("FILTER ( regex (str(?genre), '{0}', 'i'))", "crime");
+            string dateMatch = String.Format("FILTER ((?releaseDate >= '{0}-01-01'^^xsd:date) && (?releaseDate < '{0}-12-31'^^xsd:date))", 2012); ;
+
+            string queryPattern =
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+                "PREFIX db: <http://dbpedia.org/ontology/> " +
+                "PREFIX prop: <http://dbpedia.org/property/> " +
+                "SELECT ?movieLink ?title ?genreLink ?genre ?releaseDate " +
+                "WHERE {{ " +
+                    "?movieLink rdf:type db:Film; " +
+                               "foaf:name ?title. " +
+                    "OPTIONAL {{ ?movieLink prop:genre ?genreLink. " +
+                               "?genreLink rdfs:label ?genre. " +
+                               "FILTER(lang(?genre) = 'en') }}. " +
+                    "OPTIONAL {{ ?movieLink <http://dbpedia.org/ontology/releaseDate> ?releaseDate }}. " +
+
+                    "{0}" +
+                    "{1}" +
+                    "FILTER(lang(?title) = 'en') " +
+                "}}" +
+                "ORDER BY DESC(?releaseDate)" +
+                "{2}";
+            string expected = String.Format(queryPattern, genreMatch, dateMatch, limit);
+
+            LuisJSONModel data = new LuisJSONModel();
+
+            var file = Path.Combine(@"C:\Users\ieuan\Desktop\New folder\Dissertation-Project\The application\SemanticWebNPLSearchEngine.Tests\Classes\TestItems\test2LuisData.json");
+            data = JsonConvert.DeserializeObject<LuisJSONModel>(File.ReadAllText(file));
+
+
+            string output = utilities.ExtractLuisData(data);
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
