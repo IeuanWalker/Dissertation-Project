1e2e881cbf9ba48f824fc46d3577e7f69c1302c5
Ieuan Walker <Ieuan.walker007@gmail.com>
2017-07-19 16:07:08 +0100

    update

--- The application/SemanticWebNPLSearchEngine/Scripts/search.js
+++ The application/SemanticWebNPLSearchEngine/Scripts/search.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
     $('#search-input').on("keypress", function (e) {
         if (e.keyCode == 13) {
             var search = $.trim($("#search-input").val());
-
-            if (search.length > 0) {
-            }
             return false; // prevent the button click from happening
         }
     });
