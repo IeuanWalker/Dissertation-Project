4a05728bb194fe36065a7df63f0fcd82ae90c8e7
IeuanWalker <ieuan.walker007@gmail.com>
2018-04-02 17:00:09 +0100

    new .net core project

---
+++ The application/Dissertation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dissertation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
